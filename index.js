51d76b522d4fe1f4bf2f4b2c29d587eab6eb855d
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-01-12 12:07:01 +0100

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js";
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-trancker-app-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -8,19 +8,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
 
-
-let myLeads = []
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
-const tabBtn = document.getElementById("saveTab-btn")
-const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
-
-if (leadsFromLocalStorage) {
-    myLeads = leadsFromLocalStorage
-    render(myLeads)
-}
 
 function render(leads){
 let listItems= ""
@@ -34,25 +25,13 @@ for (let i = 0; i < leads.length; i++) {
 }
 ulEl.innerHTML= listItems 
 }
-inputBtn.addEventListener("click", function () {
-    myLeads.push(inputEl.value)
-    inputEl.value = ""
-    localStorage.setItem("myLeads", JSON.stringify(myLeads)) 
-    render(myLeads)
-})
-tabBtn.addEventListener("click", function () {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-        myLeads.push(tabs[0].url) 
-        localStorage.setItem("myLeads", JSON.stringify(myLeads) )
-        render(myLeads)
-    })
-})
 deleteBtn.addEventListener("dblclick", function() {
-    console.log("doble click")
-    localStorage.clear()
-    myLeads = []
-    render(myLeads)
 } )
+
+inputBtn.addEventListener("click", function () {
+    console.log(inputEl.value)
+    inputEl.value = ""
+})
 
 
 
