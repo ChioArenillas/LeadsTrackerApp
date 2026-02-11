957e376ee76404560778f246c5e1a0fb7f3f67cd
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-02-11 12:33:33 +0100

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js"
-import { getDatabase } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-trancker-app-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,6 +7,7 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
+const referenceInDB = ref (database, "leads")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
@@ -29,7 +30,7 @@ deleteBtn.addEventListener("dblclick", function() {
 } )
 
 inputBtn.addEventListener("click", function () {
-    console.log(inputEl.value)
+    push(referenceInDB, inputEl.value)
     inputEl.value = ""
 })
 
