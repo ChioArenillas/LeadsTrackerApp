23dd3e32743ac17a97231d60150f86d24e0fbdb0
Rocio Arenillas (Chio) <arenillasr@gmail.com>
2026-02-11 15:59:30 +0100

    new

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/12.7.0/firebase-database.js"
 
 const firebaseConfig = {
     databaseURL: "https://leads-trancker-app-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -7,27 +7,38 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig)
 const database = getDatabase(app)
-const referenceInDB = ref (database, "leads")
+const referenceInDB = ref(database, "leads")
 
 const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el")
 
-function render(leads){
-let listItems= ""
-for (let i = 0; i < leads.length; i++) {
-    listItems += `
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
     <li>
         <a target='_blank' href='${leads[i]}'>
             ${leads[i]}
         </a>
     </li>`
+    }
+    ulEl.innerHTML = listItems
 }
-ulEl.innerHTML= listItems 
-}
-deleteBtn.addEventListener("dblclick", function() {
-} )
+
+onValue(referenceInDB, function (snapshot) {
+    const snapshotDoesExist = snapshot.exists()
+    if (snapshotDoesExist) {
+        const snapshotValues = snapshot.val()
+        const leads = Object.values(snapshotValues)
+        render(leads)
+    }
+})
+deleteBtn.addEventListener("dblclick", function () {
+    remove(referenceInDB)
+    ulEl.innerHTML = ""
+})
 
 inputBtn.addEventListener("click", function () {
     push(referenceInDB, inputEl.value)
@@ -37,6 +48,6 @@ inputBtn.addEventListener("click", function () {
 
 
 
- 
+
 
 
